f2217825319e89c67e23f8640ce8bba5e3617bca
Sanjit <ssanjit856@gmail.com>
2022-04-28 23:53:51 +0530

    added HomPage

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import { makeServer } from "./server";
-import { store } from "./Store/Store";
 
 // Call make Server
 makeServer();
@@ -14,8 +12,6 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <BrowserRouter>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </BrowserRouter>
 );
