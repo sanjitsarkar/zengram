55c82e5b73a8710a4d081e698ebcef160c4eff87
Sanjit <ssanjit856@gmail.com>
2022-05-10 18:46:54 +0530

    removed commented code

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,35 +1,8 @@
 import React from "react";
-import {
-  MdBookmark,
-  MdExplore,
-  MdHome,
-  MdMessage,
-  MdPerson,
-} from "react-icons/md";
 import { useSideBarItem } from "../context/sideBarItemContext";
+import { sideBarItems } from "../utils";
 import SideBarItem from "./SideBarItem";
-const sideBarItems = [
-  {
-    name: "Home",
-    Icon: MdHome,
-  },
-  {
-    name: "Explore",
-    Icon: MdExplore,
-  },
-  {
-    name: "Bookmarks",
-    Icon: MdBookmark,
-  },
-  {
-    name: "Messages",
-    Icon: MdMessage,
-  },
-  {
-    name: "Profile",
-    Icon: MdPerson,
-  },
-];
+
 const SideBar = () => {
   const { activeName, setActiveName } = useSideBarItem("Home");
 
@@ -43,51 +16,6 @@ const SideBar = () => {
           onClick={() => setActiveName(activeName)}
         />
       ))}
-      {/* <li className="inline-block text-white ">
-        <Link
-          className="flex   items-center gap-2  py-6 px-4 w-48 border-b-4 border-white border-opacity-50 bg-primary rounded-r-xl"
-          to="/"
-        >
-          <MdHome className="text-4xl" />
-          <span className="text-lg">Home</span>
-        </Link>
-      </li>
-      <li className="inline-block hover:text-white focus:text-white">
-        <Link
-          className="flex   items-center gap-2 rounded-r-xl py-6 px-4 w-48 border-b-4 border-primary bg-white hover:border-b-4 hover:border-white hover:border-opacity-50 hover:bg-primary focus:border-b-4 focus:border-white focus:border-opacity-50 focus:bg-primary"
-          to="/"
-        >
-          <MdExplore className="text-4xl" />
-          <span className="text-lg">Home</span>
-        </Link>
-      </li>
-      <li className="inline-block">
-        <Link
-          className="flex   items-center gap-2 rounded-r-xl py-6 px-4 w-48 border-b-4 border-primary bg-white"
-          to="/"
-        >
-          <MdBookmark className="text-4xl" />
-          <span className="text-lg">Bookmarks</span>
-        </Link>
-      </li>
-      <li className="inline-block">
-        <Link
-          className="flex   items-center gap-2 rounded-r-xl py-6 px-4 w-48 border-b-4 border-primary bg-white  bg-white"
-          to="/"
-        >
-          <MdMessage className="text-4xl" />
-          <span className="text-lg">Messages</span>
-        </Link>
-      </li>
-      <li className="inline-block">
-        <Link
-          className="flex   items-center gap-2 rounded-r-xl py-6 px-4 w-48  bg-white"
-          to="/"
-        >
-          <MdPerson className="text-4xl" />
-          <span className="text-lg">Profile</span>
-        </Link>
-      </li> */}
     </ul>
   );
 };
---
+++ src/utils/index.js
@@ -0,0 +1,29 @@
+import {
+  MdBookmark,
+  MdExplore,
+  MdHome,
+  MdMessage,
+  MdPerson,
+} from "react-icons/md";
+export const sideBarItems = [
+  {
+    name: "Home",
+    Icon: MdHome,
+  },
+  {
+    name: "Explore",
+    Icon: MdExplore,
+  },
+  {
+    name: "Bookmarks",
+    Icon: MdBookmark,
+  },
+  {
+    name: "Messages",
+    Icon: MdMessage,
+  },
+  {
+    name: "Profile",
+    Icon: MdPerson,
+  },
+];
