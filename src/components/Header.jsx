1f20bb118be6f02b28a36ee0b36acb922f2c45ac
Sanjit <ssanjit856@gmail.com>
2022-05-10 18:40:52 +0530

    added responsive ness for header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { BiBell, BiSearch } from "react-icons/bi";
+import { MdClose, MdMenu } from "react-icons/md";
 const Header = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false);
   return (
-    <header className="fixed left-0 right-0 top-0 text-dark justify-between bg-white py-4 px-6 shadow-md flex items-center">
+    <header className="fixed z-50  left-0 right-0 top-0 text-dark justify-between bg-white py-4 px-6 shadow-md flex items-center">
       <div className="left flex items-center gap-3">
-        <div className="site-title text-xl">
+        {isNavOpen ? (
+          <MdMenu
+            className="cursor-pointer rounded-full p-2 w-10 h-10 shadow-md  sm:hidden"
+            onClick={() => setIsNavOpen(!isNavOpen)}
+          />
+        ) : (
+          <MdClose
+            className="cursor-pointer rounded-full p-2 w-10 h-10 shadow-md sm:hidden"
+            onClick={() => setIsNavOpen(!isNavOpen)}
+          />
+        )}
+        <div className="site-title text-xl  ">
           <img src="" alt="" />
           <h4>
             Zen
             <span className="text-primary">Gram</span>
           </h4>
         </div>
-        <div className="input-box ease-in-out transition-all bg-lightBlue bg-opacity-10 focus-within:bg-opacity-5 focus-within:border-opacity-50 border border-transparent focus-within:border-primary w-96  rounded-md flex  items-center">
+        <div className="input-box ease-in-out transition-all bg-lightBlue bg-opacity-10 focus-within:bg-opacity-5 focus-within:border-opacity-50 border border-transparent focus-within:border-primary sm:w-52 md:w-96  rounded-md flex  items-center">
           <BiSearch className="ml-3  text-xl text-dark" />
           <input
             type="search"
@@ -22,10 +35,14 @@ const Header = () => {
           />
         </div>
       </div>
-      <div className="right flex items-center gap-2">
-        <BiBell className="text-xl text-darkBlue cursor-pointer" />
+      <div
+        className={` right ${
+          isNavOpen ? "flex" : "hidden"
+        }  sm:flex absolute top-20 sm:h-auto h-auto sm:relative sm:border-none border-2 border-primary sm:top-0 sm:left-0 p-5 sm:p-0   bg-lightBlue sm:bg-transparent left-2 rounded-md sm:rounded-none  flex-col sm:flex-row items-center  gap-4 shadow-lg sm:shadow-none`}
+      >
+        <BiBell className="text-2xl text-white sm:text-darkBlue cursor-pointer" />
         <img
-          className="border-2 border-secondary shadow-sm cursor-pointer rounded-full w-42 h-42"
+          className="border-2 border-secondary shadow-sm cursor-pointer rounded-full w-10 h-10 "
           src="https://www.gravatar.com/avatar/94d093eda664addd6e450d7e9881bcad?s=32&d=identicon&r=PG"
           alt="Avatar"
         />
