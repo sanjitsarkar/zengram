9016faba3c09848fd2879d9e034ea252f7c13623
Sanjit <ssanjit856@gmail.com>
2022-05-16 02:44:52 +0530

    added like feature

--- src/services/likePost/likePostService.js
+++ src/services/likePost/likePostService.js
@@ -3,7 +3,6 @@ import { callApi } from "../../utils";
 
 export const likePost = createAsyncThunk("posts/likePost", async (data) => {
   const { id, likedBy } = data;
-  console.log("likePost data", data);
   const response = await callApi("put", `posts/${id}/like`, true, { likedBy });
   return response.data;
 });
@@ -12,7 +11,6 @@ export const dislikePost = createAsyncThunk(
   "posts/dislikePost",
   async (data) => {
     const { id, dislikedBy } = data;
-    console.log("dislikePost data", data);
     const response = await callApi("put", `posts/${id}/dislike`, true, {
       dislikedBy,
     });
